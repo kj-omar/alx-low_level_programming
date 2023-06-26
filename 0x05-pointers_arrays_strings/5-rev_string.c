edec5986be787d8da0536e1e31d5302afdc7f531
kj-omar <mrmihabi2004@gmail.com>
2023-06-26 15:50:19 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+void rev_string(char *s);
+{
+    int length = strlen(str);
+    int i, j;
+    char temp;
+
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+}

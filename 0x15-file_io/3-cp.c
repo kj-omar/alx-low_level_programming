7fef348f32263e65cfce9f449302d89659c5b9d0
kj-omar <mrmihabi2004@gmail.com>
2023-08-07 16:03:07 -0700

    file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,71 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
+ */
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+
+    FILE *from = fopen(argv[1], "r");
+    FILE *to = fopen(argv[2], "w");
+
+    if (!from)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+    if (!to)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        fclose(from);
+        exit(99);
+    }
+
+    char buffer[BUFFER_SIZE];
+    size_t bytesRead;
+
+    do {
+        bytesRead = fread(buffer, sizeof(char), BUFFER_SIZE, from);
+        if (ferror(from))
+        {
+            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+            fclose(from);
+            fclose(to);
+            exit(98);
+        }
+
+        size_t bytesWritten = fwrite(buffer, sizeof(char), bytesRead, to);
+        if (bytesWritten != bytesRead || ferror(to))
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            fclose(from);
+            fclose(to);
+            exit(99);
+        }
+
+    } while (bytesRead > 0);
+
+    fclose(from);
+    fclose(to);
+
+    return 0;
+}
